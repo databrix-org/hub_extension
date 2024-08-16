6c8e1d99cb5248a143424309cddf2d879e8e8c86
guyq1997 <gyq673516081@gmail.com>
2024-08-16 12:21:54 +0200

    a

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {
-  ConnectionLost,
+
   IConnectionLost,
   JupyterFrontEnd,
   JupyterFrontEndPlugin,
@@ -29,52 +29,32 @@ import { ServerConnection, ServiceManager } from '@jupyterlab/services';
      info: JupyterLab.IInfo | null
    ): IConnectionLost => {
      const trans = translator.load('jupyterlab');
-     const hubPrefix = paths.urls.hubPrefix || '';
-     const baseUrl = paths.urls.base;
 
-     // Return the default error message if not running on JupyterHub.
-     if (!hubPrefix) {
-       return ConnectionLost;
-     }
-
-     // If we are running on JupyterHub, return a dialog
-     // that prompts the user to restart their server.
-     let showingError = false;
      const onConnectionLost: IConnectionLost = async (
-       manager: ServiceManager.IManager,
-       err: ServerConnection.NetworkError
-     ): Promise<void> => {
-       if (showingError) {
-         return;
-       }
+        manager: ServiceManager.IManager,
+        err: ServerConnection.NetworkError
+      ): Promise<void> => {
 
-       showingError = true;
-       if (info) {
-         info.isConnected = false;
-       }
-       console.log('Databrix restart dialog is started!');
-       const result = await showDialog({
-         title: trans.__('Server unavailable or unreachable'),
-         body: trans.__(
-           'Your server at %1 is not running.\nxxxxxxxxxxxx?',
-           baseUrl
-         ),
-         buttons: [
-           Dialog.okButton({ label: trans.__('Restart') }),
-           Dialog.cancelButton({ label: trans.__('Dismiss') })
-         ]
-       });
+        const result = await showDialog({
+          title: trans.__('Server unavailable or unreachable'),
+          body: trans.__(
+            'Your server at %1 is not running.\nxxxxx?'
+          ),
+          buttons: [
+            Dialog.okButton({ label: trans.__('Restart') }),
+            Dialog.cancelButton({ label: trans.__('Dismiss') })
+          ]
+        });
 
-       if (info) {
-         info.isConnected = true;
-       }
-       showingError = false;
+        if (info) {
+          info.isConnected = true;
+        }
 
-       if (result.button.accept) {
-         await app.commands.execute("help:jupyter-forum");
-       }
-     };
-     return onConnectionLost;
+        if (result.button.accept) {
+          await app.commands.execute("help:jupyter-form");
+        }
+      };
+      return onConnectionLost;
    },
    autoStart: true,
    provides: IConnectionLost
